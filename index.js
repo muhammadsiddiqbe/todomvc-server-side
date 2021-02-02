52297ac2e4ad9f5b6efc7dbf9697fab136c67868
Muhammadsiddiq <muhammadsiddiqbe@gmail.com>
2021-02-02 11:55:01 +0500

    new styles

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const cors = require('cors')
 
 const PORT = process.env.PORT || 8000
 const app = express()
-let formData = []
+const formData = []
 
 app.use(express.static(path.join(__dirname , '/static/css')))
 app.use(express.static(path.join(__dirname , '/static/js')))
@@ -18,19 +18,23 @@ app.set('view engine', 'html')
 
 // GET
 app.get('/', (req, res) => {
-  res.render('home.html')
+  res.render('home.html', {formData})
 })
 
 // POST
 app.post('/', async (req, res) => {
-  const form = await parser.form(req)
+  const { todoContent } = await parser.form(req)
 
-  formData.push(form)
+  formData.push({
+    todoContent: todoContent,
+    id: uuid(),
+    removed: false
+  })
+
   console.log(formData)
 
   res.redirect('/')
 })
-
 
 
 app.listen(PORT, console.log(`Server runnning at http://localhost:${PORT}`))
